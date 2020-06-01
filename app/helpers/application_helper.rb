e4c00e06e7914f5f60457b324bc51e26f49eeb58
Wendel Lopes dos Santos Filho <wendel@pop-os.localdomain>
2020-06-01 15:53:10 -0300

    final project and jquery

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def cart_count_over_one
+    if @cart.line_items.count > 0
+      return "<span class='tag is-dark'>#{@cart.line_items.count}</span>".html_safe
+    end
+  end
+
+  def total_cart_item
+    total = @cart.line_items.map(&:quantity).sum
+    return total if total > 0
+  end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,21 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
+require("global.js")
+require("instruments.js")
+
+$(document).ready(function() {
+
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(function() {
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+
+  });
+});
 
 
 
